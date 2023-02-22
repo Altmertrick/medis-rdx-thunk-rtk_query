7a83b2f3187a687e3edf652f8124a4524856a020
Altmertrick <altmertricks@gmail.com>
2023-02-22 10:36:06 +0200

    implemented refetching Albums after addAlbum with tag system

--- src/components/AlbumsList.tsx
+++ src/components/AlbumsList.tsx
@@ -1,18 +1,26 @@
-import { useFetchAlbumsQuery } from '../store';
+import { useAddAlbumMutation, useFetchAlbumsQuery } from '../store';
 import { UserT } from '../store';
 import Skeleton from './Skeleton';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
+import classNames from 'classnames';
 
 type PropsT = {
   user: UserT;
 };
 
 const AlbumsList: React.FC<PropsT> = ({ user }) => {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isLoading, isFetching, refetch } =
+    useFetchAlbumsQuery(user);
+  const [addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = async (user: UserT) => {
+    addAlbum(user);
+  };
 
   let content;
-
   if (isLoading) {
-    content = <Skeleton times={3} />;
+    content = <Skeleton times={2} className="h-10 w-full" />;
   } else if (error) {
     //  as hook returns error of type FetchBaseQueryError | SerializedError | undefined
     // we need to narrow down its type to safely access its props
@@ -33,11 +41,35 @@ const AlbumsList: React.FC<PropsT> = ({ user }) => {
     }
   } else {
     content = data?.map((album) => {
-      return <div key={album.id}>{album.title}</div>;
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          Photos content
+        </ExpandablePanel>
+      );
     });
   }
 
-  return <div>{content}</div>;
+  const onUpdating = classNames({
+    ['bg-gray-100 opacity-60']: isFetching,
+  });
+
+  return (
+    <div>
+      <div className="flex flex-row justify-between my-3">
+        <div>Albums for {user.name}</div>
+        <Button
+          loading={results.isLoading}
+          onClick={() => {
+            handleAddAlbum(user);
+          }}
+        >
+          Add Album
+        </Button>
+      </div>
+      <div className={onUpdating}>{content}</div>
+    </div>
+  );
 };
 
 export default AlbumsList;
--- src/store/api/albumsApi.ts
+++ src/store/api/albumsApi.ts
@@ -1,11 +1,27 @@
+import { nanoid } from '@reduxjs/toolkit';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { AlbumT, UserT } from '../index';
+import { faker } from '@faker-js/faker';
 
 export const albumsApi = createApi({
   reducerPath: 'albums',
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3005/' }),
+  tagTypes: ['Albums'],
   endpoints: (builder) => ({
+    addAlbum: builder.mutation<any, UserT>({
+      query: (user) => {
+        return {
+          url: '/albums',
+          method: 'POST',
+          body: { userId: user.id, title: faker.commerce.productName() },
+        };
+      },
+      invalidatesTags: (result, error, user) => [
+        { type: 'Albums', id: user.id },
+      ],
+    }),
     fetchAlbums: builder.query<Array<AlbumT>, UserT>({
+      providesTags: (result, error, user) => [{ type: 'Albums', id: user.id }],
       query: (user) => {
         return {
           url: '/albums',
@@ -17,4 +33,4 @@ export const albumsApi = createApi({
   }),
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
